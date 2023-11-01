243b2292b822428d3784fcd82aa2c34086997bfb
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-11-01 23:05:18 +0300

    Update urls.py

--- alphonce/urls.py
+++ alphonce/urls.py
@@ -4,4 +4,5 @@ from alphonce import views
 urlpatterns = [
     path("", views.home, name='home'),
     path('/index/', views.index, name='index'),
+    path("/detail/", views.detail, name='detail'),
 ]
